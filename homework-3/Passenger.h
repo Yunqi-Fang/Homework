eea897305c561fe1242cd1735128f0e8d8350780
Sherman Wang <75777426+Yunqi-Fang@users.noreply.github.com>
2022-07-25 21:29:21 +0800

    Add files via upload

---
+++ homework-3/Passenger.h
@@ -0,0 +1,34 @@
+//
+//  Passenger.h
+//
+//  Created by Wang Shuo on 2022/7/25.
+//
+
+#import "Person.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Orders : NSObject
+
+@property (nonatomic, copy) NSString *passengerName;
+@property (nonatomic, copy) NSString *trainNumber;
+@property (nonatomic, assign) NSInteger seatNumber;
+@property (nonatomic, assign) NSDate *starttime;
+@property (nonatomic, assign) NSInteger fare;
+
+- (instancetype)initOrders:(NSString *)passengerName trainnumber:(NSString *)trainNumber seatnumber:(NSInteger)seatNumber starttime:(NSDate *)starttime fare:(NSInteger)fare
+
+@end
+
+@interface Passenger : Person
+
+@property (nonatomic, assign) BOOL ifeighteen;
+@property (nonatomic, strong) NSMutableArray *OrderHistory;
+@property(nonatomic, strong) NSMutableArray *OutstandingOrder;
+
+- (void)booking:(Orders *) order;
+- (void)checkin:(Orders *) order;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ homework-3/Passenger.m
@@ -0,0 +1,40 @@
+//
+//  Passenger.m
+//
+//  Created by Wang Shuo on 2022/7/25.
+//
+
+#import "Passenger.h"
+
+
+@implementation Orders
+
+- (instancetype)initOrders:(NSString *)passengerName trainnumber:(NSString *)trainNumber seatnumber:(NSInteger)seatNumber starttime:(NSDate *)starttime fare:(NSInteger)fare{
+    if(self = [super init]){
+        [self createOrders:passenderName trainnumber:trainNumber seatnumber:seatNumber starttime:starttime fare:fare];
+    }
+    return self;
+}
+
+-(void)createOrders:(NSString *)passengerName trainnumber:(NSString *)trainNumber seatnumber:(NSInteger)seatNumber starttime:(NSDate *)starttime fare:(NSInteger)fare{
+    self.passengerName = passengerName;
+    self.trainNumber = trainNumber;
+    self.seatNumber = seatNumber;
+    self.starttime = starttime;
+    self.fare = fare;
+}
+
+@end
+
+@implementation Passenger
+
+- (void)booking:(Orders *) order{
+    [self.OutstandingOrder addObject:order];
+}
+
+- (void)checkin:(Orders *) order{
+    [self.OutstandingOrder removeObject:order];
+    [self.OrderHistory addObject:order];
+}
+
+@end
